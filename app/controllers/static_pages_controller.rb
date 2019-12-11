dd855d8340f4cd78e9f429ec396e1b22ffb444b8
Ray980911 <rayhori0911@gmail.com>
2019-12-11 15:35:10 +0900

    change

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,14 @@
 class StaticPagesController < ApplicationController
-<<<<<<< HEAD
+
   def home
-=======
-  def hoem
->>>>>>> c6bc10aae9e155b0f47ebd37b08c8cf1bf459dae
+
   end
 
   def help
   end
-<<<<<<< HEAD
+
 
   def about
   end
-=======
->>>>>>> c6bc10aae9e155b0f47ebd37b08c8cf1bf459dae
+
 end
